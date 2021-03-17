b9b49bb7bde7bc10ed3e2d92baed7c756f9ebf34
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-03-16 20:11:41 -0400

    Time: 44 ms (14.71%), Space: 14.1 MB (81.75%) - LeetHub

--- encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
+++ encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
@@ -4,19 +4,17 @@ class Codec:
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        self.longUrl = longUrl
-        alpha = string.ascii_letters
-        digit = string.digits
-        encoded = "".join(random.sample(alpha+digit, 6))
-        if encoded not in self.en_de:
-            self.en_de['http://tinyurl.com/'+encoded] = self.longUrl
-        return 'http://tinyurl.com/'+encoded
+        alpha,digit = string.ascii_letters,string.digits
         
+        encoded = "".join(random.sample(alpha+digit, 6))
+        
+        if encoded not in self.en_de:
+            self.en_de['http://tinyurl.com/'+encoded] = longUrl
+        return 'http://tinyurl.com/'+encoded
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-
         return (self.en_de.get(shortUrl))
 
 
