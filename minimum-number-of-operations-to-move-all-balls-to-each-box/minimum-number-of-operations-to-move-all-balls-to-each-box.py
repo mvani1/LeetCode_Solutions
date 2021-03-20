4e81f0765d2ff3b429ee97aa54af5cde1b23969d
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-03-19 20:31:12 -0400

    Time: 64 ms (100.00%), Space: 14.8 MB (33.33%) - LeetHub

--- minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,14 +1,20 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        indexs = []
-        for i in range(len(boxes)):
-            if boxes[i] == "1":
-                indexs.append(i)
-        res = []
-        for j in range(len(boxes)):
-            temp = []
-            for k in indexs:
-                if k!=j:
-                    temp.append(abs(j-k))
-            res.append(sum(temp))
-        return res
+        n = len(boxes)
+        left,right = [0]*n,[0]*n
+        
+        count_1,count_2 = int(boxes[0]),int(boxes[-1])
+        
+        for i in range(1,len(boxes)):
+            left[i] = left[i-1]+count_1
+            if boxes[i] == '1':
+                count_1+=1
+        for i in range(len(boxes)-2,-1,-1):
+            right[i] = right[i+1]+count_2
+            if boxes[i] == '1':
+                count_2+=1
+        return list(map(lambda x,y:x+y,left,right))
+                
+                
+                
+            
