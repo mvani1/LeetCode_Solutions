960ea9194466bbf34fad2aa5a873b5564a76b16e
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-03-20 20:09:22 -0400

    Time: 732 ms (72.04%), Space: 18.9 MB (59.48%) - LeetHub

--- find-the-town-judge/find-the-town-judge.py
+++ find-the-town-judge/find-the-town-judge.py
@@ -1,19 +1,23 @@
 class Solution:
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
         if trust ==[] and N==1:return N
+        
         h = {}
+        out = {}
         for i in trust:
             if i[1] in h:
                 h[i[1]].append(i[0])
             else:
-                h[i[1]] =[i[0]]
-        trust_check = []
-        
-        for i in (h.values()):
-            trust_check.extend(i)
-        for i in range(1,1+N):
-            if i in h.keys() and len(h[i]) == N-1 and i not in trust_check:
-                return i
-        return -1
+                h[i[1]] = [i[0]]
+        for i in trust:
+            if i[0] in out:
                 
-                 
+                out[i[0]].append(i[1])
+            else:
+                out[i[0]] = [i[1]]
+        for i,vv in h.items():
+            # print(i,vv)
+            if len(h[i]) == N-1 and i not in out:
+                    return i
+        return -1
+        
