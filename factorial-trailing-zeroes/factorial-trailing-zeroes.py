5128038220fa8207d4e7f4c16673f52bb4b5ede1
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-03-25 19:12:19 -0400

    Time: 6060 ms (8.76%), Space: 25.3 MB (5.13%) - LeetHub

--- factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -1,11 +1,11 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
+        # memo = []
         def fact(n):
             if n ==0 or n==1:
                 return 1
-            # print(n)
             return n*fact(n-1)
         n = (fact(n))
-        # print(n)
+        
         return len(str(n))-len(str(n).strip('0'))
         
