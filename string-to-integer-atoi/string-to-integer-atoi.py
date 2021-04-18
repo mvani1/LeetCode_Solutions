4ad719ed86b8ef9b9c2bd38e5d2d31daff23b343
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-04-17 21:36:05 -0400

    Time: 44 ms (5.38%), Space: 14.3 MB (24.01%) - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.py
+++ string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,16 +1,34 @@
+maxx = (2**31)
+minn = -(2**31)-1
+        
 class Solution:
     def myAtoi(self, s: str) -> int:
-        strr = s.strip()
-        beg = re.findall("^[\+\-]?\d+", strr)
-        minn,maxx = -2**31,2**31-1
-        if beg:
-            result = int(beg[0])
-            if result>minn and result<maxx:
-                return result
-            elif result<=minn:
-                return minn
-            elif result>=maxx:
-                return maxx
+        s = s.strip()
+        if not s or s[0].isalpha():
+            return 0
+        r = ""
+        flag = 0
+        for i in s:
+            if i.isdigit():
+                flag = 1
+            if flag and not i.isdigit(): #in ['-','+','1','2','3','4','5','0','6','7','8','9']:
+                break
+            r += i
+        if '+' in r and '-' in r:
+            return 0
+        if ' ' in r and '-' in r:
+            return 0
+        if '+' in r and ' ' in r:
+            return 0
+        
+        try:    
+            if int(r)>=maxx:return maxx-1 
+            if int(r)<=minn: return minn+1
+        except:
+            return 0
+        return int(r)
+                
+            
+            
                 
         
-        return 0
