821148f5f0859fbb8afe26aa5257dc760968f8ce
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-04-20 02:51:07 -0400

    Time: 144 ms (54.50%), Space: 16.8 MB (43.27%) - LeetHub

--- max-area-of-island/max-area-of-island.py
+++ max-area-of-island/max-area-of-island.py
@@ -1,25 +1,24 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:return 0
-        self.area = 0
-        def back(x,y,grid):
-            self.area+=1
-            grid[x][y] ='-1'
-            if x+1<len(grid) and grid[x+1][y]==1:
-                back(x+1,y,grid)
-            if x-1<len(grid) and x-1>-1 and grid[x-1][y]==1:
-                back(x-1,y,grid)
-            if y-1<len(grid[0]) and y-1>-1 and grid[x][y-1]==1:
-                back(x,y-1,grid)
-            if y+1<len(grid[0]) and grid[x][y+1]==1:
-                back(x,y+1,grid)
-            
-        maxx=0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        n,m = len(grid),len(grid[0])
+        self.max = 0
+        maxx = 0
+        for i in range(n):
+            for j in range(m):
+                self.max = 0
                 if grid[i][j] == 1:
-                    back(i,j,grid)
-                    maxx = max(maxx,self.area)
-                    self.area = 0
-                    
-        return(maxx)
+                    self.back(grid,i,j)
+                    maxx = max(self.max,maxx)
+        return maxx
+    def back(self,grid,i,j,max=0):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]!=1:
+            return -1
+        grid[i][j]=0
+        self.max+=1
+        # print(max)
+        return self.back(grid,i+1,j,max)+\
+        self.back(grid,i-1,j,max)+\
+        self.back(grid,i,j+1,max)+\
+        self.back(grid,i,j-1,max)
+        
+    
