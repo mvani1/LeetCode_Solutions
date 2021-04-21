7d26b9305b0b7b139470c70198476d860127f7ef
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-04-21 01:44:37 -0400

    Time: 40 ms (64.07%), Space: 14.4 MB (27.61%) - LeetHub

--- partition-labels/partition-labels.py
+++ partition-labels/partition-labels.py
@@ -1,15 +1,20 @@
 class Solution:
     def partitionLabels(self, S: str) -> List[int]:
+        left = 0
+        right = 0
         last = {v:i for i,v in enumerate(S)}
-        idx=anchor=0
-        res = []
-        end = 0
-        while len(S)>idx:
-            start = idx
-            end = max(end,last[S[idx]]) 
-            if idx == end:
-                res.append(len(S[anchor:end+1]))
-                anchor = end+1
-            
-            idx +=1
-        return res
+        result = []
+        for i in S:
+            new = i
+            right = last[new]
+            old = left
+            while left<=right:
+                if last[S[left]] > right:
+                    right = last[S[left]]
+                left+=1
+            subs = S[old:right+1]
+            if subs:
+                result.append(len(subs))
+            right+=1
+        return result
+                    
