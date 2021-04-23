24f84f07f5aee05d7b43dd9232cf30e493204d4e
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-04-23 19:32:29 -0400

    Time: 264 ms (12.72%), Space: 14.4 MB (68.63%) - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,10 +1,19 @@
 class Solution:
+    '''
+        Time Complexity = O(n)
+    '''
     def firstUniqChar(self, s: str) -> int:
-        fre_counter = {}
-        result = 0
-        for i in s:
-            fre_counter[i] = fre_counter.get(i,0)+1
-        for j,v in fre_counter.items():
-            if v==1:
-                return s.index(j)
-        return -1
+        # boolean = [0 for _ in range(256)]
+        hash_map = {}
+        h = {}
+        minimum = (float('inf'),float('inf'))
+        visited = set()
+        for v,i in enumerate(s):
+            hash_map[i] = hash_map.get(i,0)+1
+            if hash_map[i]==1 and i not in visited:
+                h[(i,hash_map[i])] = v
+                visited.add(i)
+            elif hash_map[i]>1 and (i,hash_map[i]-1) in h and  i in visited:
+                del h[(i,hash_map[i]-1)]
+        
+        return list(h.items())[0][1] if h.items() else -1
