c11e5cdf2ec004c36cebf14233733767b7811373
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-04-24 20:46:59 -0400

    Time: 44 ms (20.91%), Space: 14.4 MB (13.37%) - LeetHub

--- two-city-scheduling/two-city-scheduling.py
+++ two-city-scheduling/two-city-scheduling.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        costs.sort(key=lambda x:(x[1]-x[0]))
-        sums = 0
-        n = len(costs)
-        for i in range(n//2):
-            sums+= costs[i][1]
-        for i in range(n//2,n):
-            sums+= costs[i][0]
-        
-        return(sums)
-        
+        costs.sort(key = lambda x:(x[1]-x[0]))
+        sumsA = 0
+        sumsB = 0
+        print(costs)
+        for i in costs[:len(costs)//2]:
+            sumsA +=i[1]
+        for i in costs[len(costs)//2:]:
+            sumsB +=i[0]
+        return sumsA+sumsB
+                   
