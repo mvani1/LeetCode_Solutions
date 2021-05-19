e0310f55d59ce786809f75ab4d0002b1ff12df07
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-05-19 19:57:03 -0400

    Time: 100 ms (67.67%), Space: 15.4 MB (91.35%) - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,5 +1,16 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        '''
+        Approach:
+
+                The idea is to do Depth First Traversal of given directed graph.
+                Start the DFS traversal from source.
+                Keep storing the visited vertices in an array or HashMap say ‘path[]’.
+                If the destination vertex is reached, get contents of path[].
+                The important thing is to mark current vertices in the path[] 
+                as visited also so that the traversal doesn’t go in a cycle.
+        
+        '''
         target = len(graph)-1
         self.ans = []
         def traverse(curr,path,t):
@@ -7,8 +18,6 @@ class Solution:
                 self.ans.append(path.copy())
                 return
             for i in graph[curr]:
-                path.append(i)
-                traverse(i,path,t)
-                path.pop()
+                traverse(i,path+[i],t)
         traverse(0,[0],target)
         return self.ans
