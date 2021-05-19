ea65b7ab1db8f482ae7dca28ee612f9345e032bd
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-05-19 14:37:41 -0400

    Time: 36 ms (94.42%), Space: 14.4 MB (92.38%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,3 +1,4 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return dict(collections.Counter(s)) == dict(collections.Counter(t))
+        return Counter(s) == Counter(t)
