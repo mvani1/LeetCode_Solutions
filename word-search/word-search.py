f38c0291811591139a5ea6e8b83cd02b7f131c93
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-05-20 03:06:45 -0400

    Time: 40 ms (95.84%), Space: 14.2 MB (74.99%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,16 +1,27 @@
 class Solution:
     def exist(self, grid: List[List[str]], word: str) -> bool:
         self.word = word
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == word[0]:
-                    if self.dfs(i,j,grid,0): return True
+        self.rows = len(grid)
+        self.cols = len(grid[0])
+        all_chars = set(word)
+        
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if grid[i][j] in all_chars:
+                    all_chars.remove(grid[i][j])
+                    
+		# exit if missing any character
+        if all_chars: return False
+        
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.dfs(i,j,grid,0): return True
         return False
                     
     def dfs(self,x,y,grid,index):
         if index == len(self.word): return True
         
-        if x == -1 or y == -1 or x == len(grid) or y == len(grid[0]) \
+        if x == -1 or y == -1 or x == self.rows or y == self.cols \
         or self.word[index] != grid[x][y]:
             return False
         
