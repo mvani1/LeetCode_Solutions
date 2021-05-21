816d21da538ab51db9f91b2992f889f76a1cc6f7
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-05-21 19:18:30 -0400

    Time: 44 ms (34.91%), Space: 14 MB (98.73%) - LeetHub

--- prison-cells-after-n-days/prison-cells-after-n-days.py
+++ prison-cells-after-n-days/prison-cells-after-n-days.py
@@ -1,4 +1,9 @@
 class Solution:
+    '''
+        We need to keep our positions in hash-table, so we can find loop_len. We do iteration by iteration and if we found a loop, we need to do (N - i) % loop_len more steps.
+
+    Complexity: time and memory is O(64), because the loop size will be not more than 64.
+    '''
     def next_step(self, cells):
         res = [0] * 8
         for i in range(1,7):
