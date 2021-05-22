449b8f98fb0521e7700135e0e83ab76e6dabc8c3
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-05-22 14:16:33 -0400

    Time: 88 ms (94.90%), Space: 18.4 MB (77.80%) - LeetHub

--- insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,41 +1,46 @@
+import random
 class RandomizedSet:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.set = []
-        self.freq = {}
-
+        
+        self._map: Dict[int] = {}
+        self._array: Array[int] = []
+        self.length = 0
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if not self.freq.get(val,0):
-            self.freq[val] = self.freq.get(val,0)+1
-            self.set.append(val)
-            return True
-        return False
+        if val in self._map:
+            return False
+        
+        self._map[val] = self.length
+        self._array.append(val)
+        self.length +=1
+        return True
+        
 
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if self.freq.get(val,0):
-            del self.freq[val]
-            self.set.remove(val)
-            return True
-        return False
+        if val not in self._map:
+            return False
+        
+        last_element, idx_val = self._array[-1], self._map[val]
+        self._array[idx_val], self._map[last_element] = last_element, idx_val
+        
+        
+        self._array.pop()
+        del self._map[val]
+        self.length -=1
+        return True
+        
 
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        return choice(self.set)
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        return random.choice(self._array)
