cb93f9854a0dd7a4a88f1d9d7389a8eb3c9a0ccf
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-05-25 19:31:59 -0400

    Time: 544 ms (99.73%), Space: 16.5 MB (66.05%) - LeetHub

--- count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
+++ count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
@@ -2,23 +2,10 @@ class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         R = len(matrix)
         C = len(matrix[0])
-        def recursion(matrix,x,y,memo):
-            if x<0 or y <0 or x>=R or y>=C or matrix[x][y]!=1:
-                return 0
-            if memo[x][y]:return memo[x][y]
-            ans = 0
-            down = recursion(matrix,x+1,y,memo)
-            right = recursion(matrix,x,y+1,memo)
-            diag = recursion(matrix,x+1,y+1,memo)
-            ans = min(down,min(right,diag)) + 1
-            memo[x][y] = ans
-            return ans
-        
         ans = 0
-        memo = [[0]*C for _ in range(R)]
-        for i in range(R):
-            for j in range(C):
-                if matrix[i][j] == 1:
-                    ans += recursion(matrix,i,j,memo)
-
-        return ans
+        for i in range(1,R):
+            for j in range(1,C):
+                if not matrix[i][j]:continue
+                if matrix[i-1][j] and matrix[i][j-1] and matrix[i-1][j-1]: 
+                    matrix[i][j] = min(matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1]) + 1
+        return sum(map(sum, matrix))
