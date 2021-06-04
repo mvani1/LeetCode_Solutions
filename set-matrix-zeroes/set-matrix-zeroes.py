66612424a3f7dcdfcf2cf115769a039f8c808e3a
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-06-03 20:35:54 -0400

    Time: 148 ms (13.27%), Space: 15.3 MB (11.40%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.py
+++ set-matrix-zeroes/set-matrix-zeroes.py
@@ -6,14 +6,66 @@ class Solution:
         row = set()
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j]==0:
-                    row.add((i,j))
-        for i,v in row:
+                if matrix[i][j] == 0:
+                    row.add((i,j))        
+        for i,j in row:
+            for k in range(len(matrix)):
+                matrix[k][j] = 0
+        
+        for i,j in row:
             for k in range(len(matrix[0])):
                 matrix[i][k] = 0
         
-        for i,v in row:
-            for k in range(len(matrix)):
-                matrix[k][v] = 0
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         row = set()
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if matrix[i][j]==0:
+#                     row.add((i,j))
+#         for i,v in row:
+#             for k in range(len(matrix[0])):
+#                 matrix[i][k] = 0
+        
+#         for i,v in row:
+#             for k in range(len(matrix)):
+#                 matrix[k][v] = 0
         
         
