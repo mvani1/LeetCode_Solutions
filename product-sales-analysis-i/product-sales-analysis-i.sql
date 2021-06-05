75b6485c4062cc691cad69c88e124a80d76c4820
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-06-05 00:44:57 -0400

    Time: 1057 ms (88.58%), Space: 0B (100.00%) - LeetHub

--- product-sales-analysis-i/product-sales-analysis-i.sql
+++ product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,4 +1,14 @@
 # Write your MySQL query statement below
+# select 
+#         p.product_name,
+#         s.year,
+#         s.price
+# from 
+#         Product p
+# join 
+#         Sales s
+# on      
+#         s.product_id = p.product_id
 select 
         p.product_name,
         s.year,
@@ -6,7 +16,14 @@ select
 from 
         Product p
 join 
-        Sales s
-on      
-        s.product_id = p.product_id
+        (select 
+                Distinct 
+                        product_id,
+                        year,
+                        price 
+                from Sales 
+        ) as s
+using (product_id);
+
+    
         
