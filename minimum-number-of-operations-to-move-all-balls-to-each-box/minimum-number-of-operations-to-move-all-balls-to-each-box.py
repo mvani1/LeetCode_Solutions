4326c3ab227fe005210a16c3c3789cd5cd11f00c
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-06-12 15:46:20 -0400

    Time: 6612 ms (12.66%), Space: 14.8 MB (9.03%) - LeetHub

--- minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,20 +1,15 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        n = len(boxes)
-        left,right = [0]*n,[0]*n
+        findZeroes = [i for i,v in enumerate(boxes)]
+        ans = [int(i) for i in boxes]
         
-        count_1,count_2 = int(boxes[0]),int(boxes[-1])
-        
-        for i in range(1,len(boxes)):
-            left[i] = left[i-1]+count_1
-            if boxes[i] == '1':
-                count_1+=1
-        for i in range(len(boxes)-2,-1,-1):
-            right[i] = right[i+1]+count_2
-            if boxes[i] == '1':
-                count_2+=1
-        return list(map(lambda x,y:x+y,left,right))
-                
-                
-                
-            
+        def findOp(initial):
+            ans = 0
+            for i,v in enumerate(boxes):
+                if i == initial : continue
+                if v == '1':
+                    ans += (abs(initial-i))
+            return ans
+        for i in findZeroes:
+            ans[i] = findOp(i)
+        return (ans)
