89a23673ccd50075a622bad66e46c1fd98247513
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-06-25 23:35:29 -0400

    Time: 1272 ms (9.13%), Space: 14.9 MB (10.94%) - LeetHub

--- maximum-units-on-a-truck/maximum-units-on-a-truck.py
+++ maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -1,14 +1,24 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        boxTypes.sort(reverse = True,key = lambda x:(x[1],x[0]))
-        print(boxTypes)
+        heap = [(-box[1],box[0]) for box in boxTypes]
+        heapq.heapify(heap)
+        # boxTypes.sort(reverse = True,key = lambda x:(x[1],x[0]))
         maxx = 0
         value = 0
-        for box,val in boxTypes:
+        while heap and maxx !=truckSize:
+            val,box = heapq.heappop(heap)
             for i in range(box):
                 maxx += 1
-                value += val
+                value += -val
                 if maxx > truckSize:
                     maxx -=1
-                    value -= val
+                    value -= -val
         return value
+        # for box,val in boxTypes:
+        #     for i in range(box):
+        #         maxx += 1
+        #         value += val
+        #         if maxx > truckSize:
+        #             maxx -=1
+        #             value -= val
+        # return value
