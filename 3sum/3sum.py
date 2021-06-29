473282f3d8f37d10c8b8f3695dbca7b0a8a2ac10
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-06-29 18:00:34 -0400

    Time: 872 ms (70.13%), Space: 18.1 MB (26.29%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,24 +1,13 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        sets = set()
-        nums.sort()
-        for i ,a in enumerate(nums):
-            if i > 0 and a == nums[i-1]:
-                continue
-            left, right = i+1, len(nums)-1
-            
-            while left<right:
-                sums = nums[i]+nums[left]+nums[right]
-                res = [nums[i],nums[left],nums[right]]
-                if sums > 0:
-                    right-=1
-                elif sums < 0:
-                    left+=1 
-                else:
-                    result.append(res)
-                    left +=1
-                    while nums[left] == nums[left-1] and left<right:
-                        left+=1
-                    
-        return (result)
+        res,dups = set(),set()
+        seen = {}
+        for i,val in enumerate(nums):
+            if val not in dups:
+                dups.add(val)
+                for ii,val2 in enumerate(nums[i+1:]):
+                    com = -val - val2
+                    if com in seen and seen[com] == i:
+                        res.add(tuple(sorted((val,val2,com))))
+                    seen[val2] = i
+        return res
