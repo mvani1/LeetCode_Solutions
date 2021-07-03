106996179bba0b314d652e244887904e6f4e78bb
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-07-03 03:33:46 -0400

    Time: 44 ms (63.53%), Space: 15.1 MB (97.31%) - LeetHub

--- design-bounded-blocking-queue/design-bounded-blocking-queue.py
+++ design-bounded-blocking-queue/design-bounded-blocking-queue.py
@@ -5,16 +5,19 @@ class BoundedBlockingQueue(object):
         self.enq = threading.Semaphore(capacity)
         self.deq = threading.Semaphore(0)
         self.queue = deque()
+        self.sizes = 0
 
     def enqueue(self, element: int) -> None:
         self.enq.acquire()
         self.queue.append(element)
         self.deq.release()
+        self.sizes+=1
 
     def dequeue(self) -> int:
         self.deq.acquire()
         ele = self.queue.popleft()
+        self.sizes-=1
         self.enq.release()
         return ele
     def size(self) -> int:
-        return len(self.queue)
+        return self.sizes
