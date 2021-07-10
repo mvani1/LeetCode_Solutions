235438eef8df0573a09c509ce9d064b512325578
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-07-10 14:56:47 -0400

    Time: 36 ms (60.24%), Space: 14.3 MB (55.55%) - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.py
+++ string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,34 +1,50 @@
 maxx = (2**31)
-minn = -(2**31)-1
+minn = -(2**31)
         
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.strip()
-        if not s or s[0].isalpha():
-            return 0
-        r = ""
-        flag = 0
-        for i in s:
-            if i.isdigit():
-                flag = 1
-            if flag and not i.isdigit(): #in ['-','+','1','2','3','4','5','0','6','7','8','9']:
-                break
-            r += i
-        if '+' in r and '-' in r:
-            return 0
-        if ' ' in r and '-' in r:
-            return 0
-        if '+' in r and ' ' in r:
+        if not s:
             return 0
         
-        try:    
-            if int(r)>=maxx:return maxx-1 
-            if int(r)<=minn: return minn+1
-        except:
+        s = s.lstrip()
+        if not s:
             return 0
-        return int(r)
+        
+        pn = False # postivie 
+        res = '' 
+        
+        
+        if s[0] == '-':
+            pn = True
+        elif s[0] == '+':
+            pn = False
+        elif not s[0].isdigit():
+            return 0
+        else:
+            res +=s[0]
+            
+        for i in range(1, len(s)):
+            if not s[i].isdigit():
+                break
+            else:
+                res +=s[i]
+        if res == "":
+            return 0
+        
+        res_int = 0
+        if pn == False:
+            res_int = int(res)
+        else:
+            res_int = -int(res)
+            
+        if res_int  >= minn and res_int < maxx-1:
+            int_res = res_int 
+        elif res_int  <= minn:
+            int_res = minn
+        else:
+            int_res = maxx-1
+            
                 
-            
-            
+        return int_res
                 
         
