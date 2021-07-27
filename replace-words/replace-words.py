3e27f78a177894404dbf8c6c4201be78524ac25f
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-07-27 01:13:28 -0700

    Time: 68 ms (92.94%), Space: 28.4 MB (52.29%) - LeetHub

--- replace-words/replace-words.py
+++ replace-words/replace-words.py
@@ -1,17 +1,28 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        Trie = lambda: defaultdict(Trie)
-        trie = Trie()
-        END = True
+        trie = {}
+        result = []
         
-        for dictt in dictionary:
-            reduce(dict.__getitem__,dictt,trie)[END] = dictt
-        def replace(word):
-            node = trie
+        for word in dictionary:
+            root = trie
             for char in word:
-                if char not in node or END in node:break
-                node = node[char]
-            return node.get(END,word)
+                if not (char in root):
+                    root[char] = {}
+                root = root[char]
+            root['$'] = word
         
-        return " ".join(map(replace,sentence.split()))
-            
+        for i in sentence.split():
+            root = trie
+            flag = 0
+            for char in i:
+                if char not in root:
+                    break
+                root = root[char]
+                if '$' in root:
+                    result.append(root['$'])
+                    flag = 1
+                    break
+            if not flag:
+                result.append(i)
+        return " ".join(result)
+        
