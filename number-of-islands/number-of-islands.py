e8b802cf03feaec1f7cc338fcb99977113e8a68f
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-08-13 14:46:56 -0700

    Time: 183 ms (13.07%), Space: 15.5 MB (39.10%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,21 +1,16 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        self.n,self.m = len(grid),len(grid[0])
-        self.seen = set()
-        self.grid = grid
+        self.m,self.n = len(grid),len(grid[0])
         count = 0
-        for i in range(self.n):
-            for j in range(self.m):
-                if grid[i][j] == "1" and (i,j) not in self.seen:
-                    self.dfs(i,j)
+        for i in range(self.m):
+            for j in range(self.n):
+                if grid[i][j] == "1" :
+                    self.dfs(grid,i,j)
                     count+=1
         return count
-    def dfs(self, x, y):
-        if x<0 or y<0 or x>=self.n or y>=self.m or self.grid[x][y]!="1" or (x,y) in self.seen:
-            return
-        self.seen.add((x,y))
-        for xx,yy in [[0,1],[1,0],[-1,0],[0,-1]]:
-            new_x,new_y = x+xx,y+yy
-            self.dfs(new_x,new_y)
-    
-        
+    def dfs(self, grid, x, y):
+        grid[x][y]='X'
+        if x+1<self.m and grid[x+1][y] == "1": self.dfs(grid,x+1,y)
+        if y+1<self.n and grid[x][y+1] == "1": self.dfs(grid,x,y+1)            
+        if x-1>=0 and grid[x-1][y] == "1": self.dfs(grid,x-1,y)            
+        if y-1>=0 and grid[x][y-1] == "1": self.dfs(grid,x,y-1)            
