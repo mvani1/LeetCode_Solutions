633b273dd0058d30fab28a48b9c7a1b35de27912
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-08-13 14:34:04 -0700

    Time: 172 ms (18.63%), Space: 19.6 MB (8.68%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,22 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        self.m = len(grid)
-        self.n = len(grid[0])
-        self.islands = 0
-        for r in range(self.m):
-            for c in range(self.n):
-                if grid[r][c] == '1':
-                    self.islands+=1
-                    self.backtrack(grid,r,c)
-        return self.islands
-    
-    def backtrack(self,grid,r,c):
-        if r<0 or c<0 or c>=self.n or r>=self.m or grid[r][c]!='1':
+        self.n,self.m = len(grid),len(grid[0])
+        self.seen = set()
+        self.grid = grid
+        count = 0
+        for i in range(self.n):
+            for j in range(self.m):
+                if grid[i][j] == "1" and (i,j) not in self.seen:
+                    self.dfs(i,j)
+                    count+=1
+        return count
+    def dfs(self, x, y):
+        if x<0 or y<0 or x>=self.n or y>=self.m or self.grid[x][y]!="1" or (x,y) in self.seen:
             return
-        grid[r][c]='0'
-        self.backtrack(grid,r+1,c)
-        self.backtrack(grid,r-1,c)
-        self.backtrack(grid,r,c-1)
-        self.backtrack(grid,r,c+1)
-        
+        self.seen.add((x,y))
+        for xx,yy in [[0,1],[1,0],[-1,0],[0,-1]]:
+            new_x,new_y = x+xx,y+yy
+            self.dfs(new_x,new_y)
+    
         
