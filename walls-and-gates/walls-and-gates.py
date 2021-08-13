d9a7e939b17b912bc6101c793b41ce998490980f
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-08-13 13:22:03 -0700

    Time: 300 ms (46.45%), Space: 16.8 MB (79.71%) - LeetHub

--- walls-and-gates/walls-and-gates.py
+++ walls-and-gates/walls-and-gates.py
@@ -1,21 +1,20 @@
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        if not rooms: return
-        q = []
-        m, n = len(rooms), len(rooms[0])
-        for i in range(m):
-            for j in range(n):
-                if not rooms[i][j]:
-                    q.append((i, j))
-        dis = 1                    
-        while q:
-            tmp_q = []
-            while q:
-                i, j = q.pop()
-                for di, dj in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-                    ii, jj = i+di, j+dj
-                    if 0 <= ii < m and 0 <= jj < n and rooms[ii][jj] == 2147483647:
-                        rooms[ii][jj] = dis 
-                        tmp_q.append((ii, jj))
-            q = tmp_q             
-            dis += 1
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        INF = 2147483647
+        q = deque()
+        n,m = len(rooms),len(rooms[0])
+        for i in range(n):
+            for j in range(m):
+                if rooms[i][j] == 0:
+                    q.append((i,j))
+        
+        while q :
+            x,y = q.popleft()
+            for xx,yy in directions:
+                new_x,new_y = xx+x, yy+y
+                if new_x<0 or new_y<0 or new_x>=n or new_y>=m or rooms[new_x][new_y]!=INF:
+                    continue
+                rooms[new_x][new_y] = rooms[x][y] + 1
+                q.append((new_x,new_y))
+                    
