251fb3aa37787861938b97210dfc28ead99be2df
mvani1 B00810186 <66803458+mvani1@users.noreply.github.com>
2021-08-28 14:36:25 -0700

    Time: 896 ms (64.78%), Space: 18.1 MB (19.56%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,13 +1,14 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res,dups = set(),set()
+        res = set()
+        dups = set()
         seen = {}
-        for i,val in enumerate(nums):
+        for i, val in enumerate(nums):
             if val not in dups:
                 dups.add(val)
-                for ii,val2 in enumerate(nums[i+1:]):
-                    com = -val - val2
-                    if com in seen and seen[com] == i:
-                        res.add(tuple(sorted((val,val2,com))))
-                    seen[val2] = i
+                for j in range(i+1,len(nums)):
+                    comp = - val - nums[j]
+                    if comp in seen and seen[comp] ==i:
+                        res.add(tuple(sorted((val,nums[j],comp))))
+                    seen[nums[j]] = i
         return res
